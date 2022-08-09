4ef8813afb6a57b2f87a7f417ede483257e41762
matszach <lkaszubowski@gmail.com>
2022-08-09 19:42:33 +0200

    +

--- src/util/dstruct/Iterator.gd
+++ src/util/dstruct/Iterator.gd
@@ -7,8 +7,17 @@ var _pointer: int = 0
 func _init(items: Array):
 	 _items = items
 	
-func current():
+func current() -> Object:
 	return _items[_pointer]
 
-func next():
-	
+func next() -> Object:
+	_pointer += 1
+	if _pointer >= _items.size():
+		_pointer = 0
+	return current()
+
+func prev() -> Object:
+	_pointer -= 1
+	if _pointer <= 0:
+		_pointer = _items.size() - 1
+	return current()
