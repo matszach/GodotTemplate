c610e4f9e97992ebcbcefcbf95a2aa513a36ac0f
matszach <lkaszubowski@gmail.com>
2022-08-09 21:17:31 +0200

    weighted pick

--- src/util/RNG.gd
+++ src/util/RNG.gd
@@ -3,3 +3,14 @@ class_name RNG
 
 func pick(array: Array) -> Object:
 	return array[randi_range(0, array.size() - 1)]
+
+func weighted_pick(array: Array) -> Object:
+	var total_weight = 0
+	for item in array:
+		total_weight += item[0]
+	var p = randf_range(0, total_weight)
+	for item in array:
+		p -= item[0]
+		if p <= 0:
+			return item
+	return null
