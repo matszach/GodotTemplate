2d971a4a2c1c24ec9e3db9e9c8ef425715a63d9d
matszach <lkaszubowski@gmail.com>
2022-08-09 20:37:38 +0200

    +

--- src/util/InputHelper.gd
+++ src/util/InputHelper.gd
@@ -4,7 +4,7 @@ class_name InputHelper
 static func ui_dir() -> Vector2:
 	return Vector2(
 		Input.get_action_strength("ui_right") -
-		Input.get_action_strength("ui_left") ,
+		Input.get_action_strength("ui_left"),
 		Input.get_action_strength("ui_up") -
 		Input.get_action_strength("ui_down") 
 	)
