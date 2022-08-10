a7facb26fe62010da134939dfdb5dd7a5d0da891
matszach <lkaszubowski@gmail.com>
2022-08-10 20:40:58 +0200

    +

--- src/autoload/Main.gd
+++ src/autoload/Main.gd
@@ -3,3 +3,6 @@ extends Node
 func _ready() -> void:
 	Logger.info('game started')
 	SaveState.load_data()
+
+func _process(delta: float) -> void:
+	print(InputHelper.ui_dir())
--- src/util/InputHelper.gd
+++ src/util/InputHelper.gd
@@ -5,6 +5,6 @@ static func ui_dir() -> Vector2:
 	return Vector2(
 		Input.get_action_strength("ui_right") -
 		Input.get_action_strength("ui_left"),
-		Input.get_action_strength("ui_up") -
-		Input.get_action_strength("ui_down") 
+		Input.get_action_strength("ui_down") -
+		Input.get_action_strength("ui_up") 
 	)
